3b912b282dd04dd6a40f4a264b04070e31529417
Daniel <dev.dc@pm.me>
2022-02-20 18:27:07 +0100

    add random data selection and response filtering

--- scraper/main.py
+++ scraper/main.py
@@ -15,19 +15,27 @@ PW = os.getenv('LINKEDIN_PASSWORD')
 # login, search url as well as search words and post text DOM element
 linkedin_login_url = 'https://www.linkedin.com/login'
 linkedin_search_root_url = 'https://www.linkedin.com/search/results/content/?keywords='       
-search_words = ['forbes30under30']
+search_words = ['forbes30under30', 'Marketing', 'ceolunch']
 post_element_class = 'feed-shared-update-v2__commentary'
 
 # tokens for data privacy filter
 tokens = ['PROPN'] # PROPN = proper noun
 
-# execute crawler
+# login to LinkedIn
 linkedin_scraper = scrp.scraper(chrome_driver_path,
-                           linkedin_login_url,
-                           linkedin_search_root_url)
+                                linkedin_login_url,
+                                linkedin_search_root_url)
 linkedin_scraper.login(USER, PW)
-linkedin_scraper.get_post_text(search_words[0], post_element_class, 3)
-linkedin_scraper.clean_texts(tokens)
-linkedin_scraper.save_clean_text(data_path)
 
-print('scraper successful')
+for word in search_words:
+    # reset previous results of loop
+    linkedin_scraper.reset_results()
+
+    # execute crawling
+    linkedin_scraper.get_post_text(word, post_element_class, 10)
+    linkedin_scraper.clean_texts(tokens)
+    linkedin_scraper.keep_language_texts('en')
+    linkedin_scraper.save_clean_text(data_path)
+    
+    n_posts = len(linkedin_scraper.all_texts_clean)
+    print(f'>>> scraper successful: found {n_posts} posts for "{word}" <<<')
