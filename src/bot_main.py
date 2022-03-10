f88c8269b36aee27b0ee549d1eeefe0e144349c5
danczw <dev.dc@pm.me>
2022-03-11 00:04:56 +0100

    add post creation on personal page

--- src/bot_main.py
+++ src/bot_main.py
@@ -1,7 +1,8 @@
 from blob import blob
+from bot_nlp import nlp_bot
 from dotenv import load_dotenv
 import os
-from bot_nlp import nlp_bot
+import requests
 
 # load env variables and set api key
 load_dotenv()
@@ -25,4 +26,32 @@ selected_run_texts = azure_blob.selected_run_texts
 openai_bot = nlp_bot(OPEN_AI_KEY, selected_run_texts, selected_run)
 openai_bot.create_text(250, 1, 0.9)
 
-print(openai_bot.new_text)
+# post to linkedin
+PROFILE_ID = os.getenv('PROFILE_ID')
+ACCESS_TOKEN = os.getenv('ACCESS_TOKEN')
+
+url = 'https://api.linkedin.com/v2/ugcPosts'
+comment = f'{openai_bot.new_text}\n\nkey word: {openai_bot.search_word.split("_")[0]}'
+
+headers = {'Content-Type': 'application/json',
+           'X-Restli-Protocol-Version': '2.0.0',
+           'Authorization': 'Bearer ' + ACCESS_TOKEN}
+
+post_data = {
+    'author': 'urn:li:person:'+PROFILE_ID,
+    'lifecycleState': 'PUBLISHED',
+    'specificContent': {
+        'com.linkedin.ugc.ShareContent': {
+            'shareCommentary': {
+                'text': comment  
+            },
+            'shareMediaCategory': 'NONE'
+        }
+    },
+    'visibility': {
+        'com.linkedin.ugc.MemberNetworkVisibility': 'PUBLIC'
+    }
+}
+
+response = requests.post(url, headers=headers, json=post_data)
+print(response)
