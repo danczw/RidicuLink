3201f42fcf22e8fb796a47b5c2256057d63d52db
danczw <dev.dc@pm.me>
2022-04-18 23:40:25 +0200

    refactor for azure function deployment

--- bot/linfluenc_time_trigger/__init__.py
+++ bot/linfluenc_time_trigger/__init__.py
@@ -5,7 +5,6 @@ import azure.functions as func
 
 from linfluenc_time_trigger.blob import blob
 from linfluenc_time_trigger.bot_nlp import nlp_bot
-from dotenv import load_dotenv
 import os
 import requests
 
@@ -17,11 +16,12 @@ def main(mytimer: func.TimerRequest) -> None:
         logging.info('The timer is past due!')
 
     logging.info('Python timer trigger function ran at %s', utc_timestamp)
-
-    # load env variables and set api key
-    load_dotenv()
-    OPEN_AI_KEY = os.getenv('OPENAI_API_KEY')
-    AZURE_CON_STRING = os.getenv('AZURE_CON_STRING')
+    
+    # load env variables
+    LINKEDIN_ORG_ID = os.environ['LINKEDIN_ORG_ID']
+    LINKEDIN_ACCESS_TOKEN = os.environ['LINKEDIN_ACCESS_TOKEN']
+    OPENAI_API_KEY = os.environ['OPENAI_API_KEY']
+    BLOB_CON_STRING = os.environ['BLOB_CON_STRING']
 
     # Azure Blob Storage container name
     container_name = 'linfluenc-parsed-texts'
@@ -29,7 +29,7 @@ def main(mytimer: func.TimerRequest) -> None:
     file_name = 'parsed_text.json'
 
     # init azure connections
-    azure_blob = blob(AZURE_CON_STRING, container_name, file_name)
+    azure_blob = blob(BLOB_CON_STRING, container_name, file_name)
 
     # load data
     azure_blob.load_rand_texts(0.7)
@@ -37,13 +37,10 @@ def main(mytimer: func.TimerRequest) -> None:
     selected_run_texts = azure_blob.selected_run_texts
 
     # create new post
-    openai_bot = nlp_bot(OPEN_AI_KEY, selected_run_texts, selected_run)
+    openai_bot = nlp_bot(OPENAI_API_KEY, selected_run_texts, selected_run)
     openai_bot.create_text(250, 1, 0.9)
 
     # post to linkedin
-    ORGANIZATION_ID = os.getenv('ORGANIZATION_ID')
-    ACCESS_TOKEN = os.getenv('ACCESS_TOKEN')
-
     url = 'https://api.linkedin.com/v2/ugcPosts'
     comment = f'''{openai_bot.new_text}\n
                 =========================\n
@@ -51,10 +48,10 @@ def main(mytimer: func.TimerRequest) -> None:
 
     headers = {'Content-Type': 'application/json',
             'X-Restli-Protocol-Version': '2.0.0',
-            'Authorization': 'Bearer ' + ACCESS_TOKEN}
+            'Authorization': 'Bearer ' + LINKEDIN_ACCESS_TOKEN}
 
     post_data = {
-        'author': 'urn:li:organization:' + ORGANIZATION_ID,
+        'author': 'urn:li:organization:' + LINKEDIN_ORG_ID,
         'lifecycleState': 'PUBLISHED',
         'specificContent': {
             'com.linkedin.ugc.ShareContent': {
