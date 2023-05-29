935f85df41a7105429df6a4e56435f9420eb7db2
danczw <dev.dc@pm.me>
2023-05-29 21:40:31 +0200

    refactor scraper without azure dependency

--- scraper/main.py
+++ scraper/main.py
@@ -1,57 +1,67 @@
-from blob import blob
-from dotenv import load_dotenv
 import os
+
+from dotenv import load_dotenv
+
 import scraper as scrp
 
-# webdriver path and file name
-chrome_driver_path = './scraper/chromedriver.exe' # TODO: change to your path
 
-# load env variables
-load_dotenv()
-USER = os.getenv('LINKEDIN_USER')
-PW = os.getenv('LINKEDIN_PASSWORD')
-BLOB_CON_STRING = os.getenv('BLOB_CON_STRING')
+def main():
+    # webdriver path and type: [chrome, firefox, edge]
+    # TODO: change according to your setup
+    selenium_driver_type = "chrome"
+    selenium_driver_path = "./scraper/chromedriver"
+    # TODO: optional, only needed if you want to use a specific browser binary, set to "" otherwise
+    binary_location = ""
 
-# login, search url as well as search words and text DOM element
-linkedin_login_url = 'https://www.linkedin.com/login'
-linkedin_search_root_url = 'https://www.linkedin.com/feed/hashtag/'
-text_element_class = 'feed-shared-update-v2__commentary'
-# TODO: update search words       
-search_words = ['beavoicenotanecho', 'linkedin', 'thoughtleadership', 'thoughtleader']
+    # TODO: update search words
+    search_words = [
+        "ChatGPT",
+    ]
 
-# tokens for data privacy filter
-tokens = ['PROPN'] # PROPN = proper noun
+    # load env variables
+    load_dotenv()
+    USER = os.getenv("LINKEDIN_USER")
+    PW = os.getenv("LINKEDIN_PASSWORD")
+    # check if env variables are set
+    if USER is None or PW is None:
+        raise ValueError("Please set environment variables LINKEDIN_USER and LINKEDIN_PASSWORD")
 
-# Azure Blob Storage container name
-container_name = 'linfluenc-parsed-texts'
+    # login, search url as well as search words and text DOM element
+    linkedin_login_url = "https://www.linkedin.com/login"
+    linkedin_search_root_url = "https://www.linkedin.com/feed/hashtag/"
+    text_element_class = "feed-shared-update-v2__commentary"
 
-# blob name
-file_name = 'parsed_text.json'
+    # login to LinkedIn
+    linkedin_scraper = scrp.scraper(
+        driver_type=selenium_driver_type,
+        driver_path=selenium_driver_path,
+        login_url=linkedin_login_url,
+        search_root_url=linkedin_search_root_url,
+        browser_binary_location=binary_location,
+    )
+    linkedin_scraper.login(USER, PW)
 
-# login to LinkedIn
-linkedin_scraper = scrp.scraper(chrome_driver_path,
-                                linkedin_login_url,
-                                linkedin_search_root_url)
-linkedin_scraper.login(USER, PW)
+    # tokens for data privacy filter
+    tokens = ["PROPN"]  # PROPN = proper noun
 
-# init azure connections
-azure_blob = blob(BLOB_CON_STRING, container_name, file_name)
+    for word in search_words:
+        # reset previous results of loop
+        linkedin_scraper.reset_results()
 
-for word in search_words:
-    # reset previous results of loop
-    linkedin_scraper.reset_results()
+        # execute crawling
+        linkedin_scraper.get_text(word.lower(), text_element_class, 5)
+        linkedin_scraper.clean_texts(tokens)
+        linkedin_scraper.keep_language_texts("en")
 
-    # execute crawling
-    linkedin_scraper.get_text(word.lower(), text_element_class, 10)
-    linkedin_scraper.clean_texts(tokens)
-    linkedin_scraper.keep_language_texts('en')
+        n_texts = len(linkedin_scraper.all_texts_clean)
+        print(f'>>> scraper successful: found {n_texts} texts for "{word}" <<<')
 
-    n_texts = len(linkedin_scraper.all_texts_clean)
-    print(f'>>> scraper successful: found {n_texts} texts for "{word}" <<<')
+        # save results
+        # TODO: implement
 
-    # upload new data
-    azure_blob.upload_texts(linkedin_scraper.all_texts_clean,
-                            linkedin_scraper.search_word)
+    # close connection
+    linkedin_scraper.close_connection()
 
-# close connection
-linkedin_scraper.close_connection()
+
+if __name__ == "__main__":
+    main()
--- scraper/scraper.py
+++ scraper/scraper.py
@@ -1,57 +1,88 @@
-import json
-import langdetect
 import os
-from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
-import spacy
 import time
 
-class scraper:
+import spacy
+from langdetect import detect
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.firefox.options import Options
 
-    def __init__(self, chrome_driver_path, login_url, search_root_url):        
-        self.driver = webdriver.Chrome(executable_path=chrome_driver_path)
+
+class scraper:
+    def __init__(
+        self,
+        driver_type: str,
+        driver_path: str,
+        login_url: str,
+        search_root_url: str,
+        browser_binary_location: str = "",
+    ):
+        self.driver = self.get_web_driver(
+            driver_type=driver_type, driver_path=driver_path, browser_binary_location=browser_binary_location
+        )
         self.login_url = login_url
         self.search_root_url = search_root_url
-        self.search_word = ''
+        self.search_word = ""
         self.all_posts = []
         self.all_texts = []
         self.all_texts_clean = []
-    
+
+    def get_web_driver(self, driver_type: str, driver_path: str, browser_binary_location: str):
+        """Get the web driver for selenium
+
+        Args:
+            driver_type (str): type of driver: [chrome, firefox, edge]
+            driver_path (str): path to the driver
+            binary_location (str): path to the browser binary
+            browser_binary_location (str, optional): path to the browser binary. Defaults to "".
+        """
+        # check if file exists
+        if not os.path.isfile(driver_path):
+            raise ValueError(f"driver_path {driver_path} does not exist, please check if file exists")
+
+        # set options for binary location if provided
+        options = Options()
+        if browser_binary_location:
+            options.binary_location = browser_binary_location
+
+        # check if driver type is supported and return driver
+        match driver_type:
+            case "chrome":
+                return webdriver.Chrome(executable_path=driver_path)
+            case "firefox":
+                return webdriver.Firefox(options=options, executable_path=driver_path)
+            case "edge":
+                return webdriver.Edge(executable_path=driver_path)
+            case _:
+                raise ValueError(f"driver_type {driver_type} not supported, please choose from [chrome, firefox, edge]")
+
     def login(self, user: str, password: str):
-        '''
-        Login to url with provided user name and password
+        """Login to url with provided user name and password
 
-        Parameter
-        ---------
-        user : str
-            user name as needed for login
-        password : str
-            password as needed for login
-        '''
-        self.driver.get(self.login_url)
-        time.sleep(3)
-        self.driver.find_element_by_id('username').send_keys(user)
-        self.driver.find_element_by_id('password').send_keys(password)
-        self.driver.find_element_by_id('password').send_keys(Keys.RETURN)
-    
-    
+        Args:
+            user (str): user name as needed for login
+            password (str): password as needed for login
+        """
+        try:
+            self.driver.get(self.login_url)
+            time.sleep(3)
+            self.driver.find_element(By.ID, "username").send_keys(user)
+            self.driver.find_element(By.ID, "password").send_keys(password)
+            self.driver.find_element(By.ID, "password").send_keys(Keys.RETURN)
+        except Exception as e:
+            print(f"Login failed: {e}")
+
     def get_text(self, search_word: str, dom_element: str, iterations: int):
-        '''
-        Scrape LinkedIn texts of posts found by a specified word
+        """Scrape LinkedIn texts of posts found by a specified word
 
-        Parameter
-        ---------
-        website : str
-            root url for searching posts by word
-        search_root_url : str
-            word to search posts for
-        dom_element : str
-            DOM element in LinkedIn containing post text
-        iterations : int
-            number of iterations to scroll though posts found
-            initially, only first few search results are loaded and found by selenium
-            the higher the iteration, the more post texts can potentially be scraped
-        '''
+        Args:
+            search_word (str): Keyword / hashtag to search posts for
+            dom_element (str): DOM element in LinkedIn containing post text
+            iterations (int): Number of iterations to scroll though posts found. Initially, only first few search
+                results are loaded by LinkedIn and found by selenium. The higher the iteration, the more post texts
+                can potentially be scraped.
+        """
         self.search_word = search_word
         time.sleep(3)
         self.driver.get(self.search_root_url + search_word)
@@ -59,9 +90,9 @@ class scraper:
 
         # iterate through posts to load more
         for i in range(iterations):
-        # get a list of all text elements from post by HTML element class name
-            self.all_posts = self.driver.find_elements_by_class_name(dom_element)
-        
+            # get a list of all text elements from post by HTML element class name
+            self.all_posts = self.driver.find_elements(By.CLASS_NAME, dom_element)
+
             # executes JavaScript to scroll the last found div into view
             self.driver.execute_script("arguments[0].scrollIntoView();", self.all_posts[-1])
             time.sleep(1)
@@ -70,81 +101,68 @@ class scraper:
         for post in self.all_posts:
             # get post text
             self.all_texts.append(post.text)
-    
-    def clean_texts(self, tokens: list):
-        '''
-        Clean text of data privacy related content (e.g. names)
 
-        Parameter
-        ---------
-        tokens : list[str]
-            list of str tokens to be replaced
-            spacey part of speech tags, used for identifying privacy related
-                data, found here: https://universaldependencies.org/docs/u/pos/
-        '''
+    def clean_texts(self, tokens: list):
+        """Clean text of data privacy related content (e.g. names)
+
+        Args:
+            tokens (list): List of str tokens to be replaced. Spacey part of speech tags, used for identifying privacy
+                related data, found here: https://universaldependencies.org/docs/u/pos/
+        """
         # load nlp model for cleanin data privacy related text content
-        nlp = spacy.load('nl_core_news_sm')
+        nlp = spacy.load("en_core_web_sm")
 
         # iterate through previously scraped texts
         for text in self.all_texts:
             # parse text to list of tokens
             doc = nlp(text)
 
-            filtered_text = ''
+            filtered_text = ""
             for token in doc:
                 # replace word if word classified as specific tokens
-                # TODO: review and expand
                 if token.pos_ in tokens:
                     # new_token = f' <{token.ent_type_}>'
-                    new_token = ''
+                    new_token = ""
                 else:
-                    new_token = f' {token.text}'
-                
+                    new_token = f" {token.text}"
+
                 # concatenate
                 filtered_text += new_token
-            
+
             # remove leading space and new lines
-            filtered_text = filtered_text[1:]
-            filtered_text = filtered_text.replace('\n', '')
-
+            filtered_text = filtered_text.replace("\n", "").strip()
             self.all_texts_clean.append(filtered_text)
-            # print(filtered_text)
 
-    def keep_language_texts(self, lang='en'):
-        '''
-        Keep all texts from cleaned texts wich are the specified language
-        
-        Parameter
-        ---------
-        lang : str
-            language which texts to keep, default is english 
-        '''
+    def keep_language_texts(self, lang: str = "en"):
+        """Keep all texts from cleaned texts which are the specified language
+
+        Args:
+            lang (str, optional): language which texts to keep, default is english
+        """
         all_texts = self.all_texts_clean
         all_texts_filtered = []
 
         for text in all_texts:
             try:
-                detected_lang = langdetect.detect(text)
-            
-                if detected_lang == lang:
+                # detect language of text
+                lang = detect(text)
+
+                # keep text if language is english
+                if lang == "en":
                     all_texts_filtered.append(text)
-            
-            except langdetect.lang_detect_exception.LangDetectException:
-                pass
-        
+            except Exception as e:
+                print(f"Error: {e}")
+                print(f"Text: {text}\n")
+
         self.all_texts_clean = all_texts_filtered
 
     def reset_results(self):
-        '''
-        Reset crawled results saved in current class object
-        '''
-        self.search_word = ''
+        """Reset crawled results saved in current class object"""
+        self.search_word = ""
         self.all_posts = []
         self.all_texts = []
         self.all_texts_clean = []
 
     def close_connection(self):
-        '''
-        Close WebDriver session
-        '''
+        """Close WebDriver session"""
         self.driver.quit()
